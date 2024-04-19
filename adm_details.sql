62c71c83862575a32534a38c313ea49f478d9602
NickyYiningChen <113316655+NickyYiningChen@users.noreply.github.com>
2024-04-19 19:17:05 -0400

    Update adm_details.sql

--- adm_details.sql
+++ adm_details.sql
@@ -1,4 +1,20 @@
-  select p.subject_id, p.gender, p.dob, p.dod, hadm_id, admittime, dischtime, admission_type, insurance, marital_status, ethnicity, hospital_expire_flag, has_chartevents_data
-    from `physionet-data.mimiciii_clinical.admissions` adm
-    join `physionet-data.mimiciii_clinical.patients` p
-    on adm.subject_id = p.subject_id
+SELECT 
+    p.subject_id, 
+    p.gender, 
+    p.dob, 
+    p.dod, 
+    adm.hadm_id, 
+    adm.admittime, 
+    adm.dischtime, 
+    adm.admission_type, 
+    adm.insurance, 
+    adm.marital_status, 
+    adm.ethnicity, 
+    adm.hospital_expire_flag, 
+    adm.has_chartevents_data
+FROM `physionet-data.mimiciii_clinical.admissions` adm
+JOIN `physionet-data.mimiciii_clinical.patients` p
+    ON adm.subject_id = p.subject_id
+JOIN `physionet-data.mimiciii_clinical.diagnoses_icd` d
+    ON adm.hadm_id = d.hadm_id
+WHERE d.icd9_code IN ('99592', '78552')
